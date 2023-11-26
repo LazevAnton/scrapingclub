77331a58d7bc5ba3b45c5b0e220012573cf13bcb
alazev <a.lazev@transdata.no>
2023-11-26 02:22:09 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,22 @@
+COOKIES = {
+    '_ga': 'GA1.1.424535848.1700947937',
+    '_ga_BD9ZHFE1XX': 'GS1.1.1700947937.1.1.1700947963.0.0.0',
+}
+
+HEADERS = {
+    'authority': 'scrapingclub.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6,no;q=0.5',
+    'cache-control': 'max-age=0',
+    # 'cookie': '_ga=GA1.1.424535848.1700947937; _ga_BD9ZHFE1XX=GS1.1.1700947937.1.1.1700947963.0.0.0',
+    'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+}
+DETAIL_URL = 'https://scrapingclub.com'
---
+++ main.py
@@ -0,0 +1,36 @@
+import requests, lxml
+from bs4 import BeautifulSoup
+from config import COOKIES, HEADERS, DETAIL_URL
+
+def get_data():
+    print('Начинаем сбор данных')
+    cards_detail = []
+    for page in range(1,7):
+        try:
+            session = requests.Session()
+            responce = session.get(
+                url='https://scrapingclub.com/exercise/list_basic/',
+                cookies=COOKIES,
+                headers=HEADERS
+            )
+        except Exception as data_er:
+            print(f'Что то пошло не так\
+                Код запроса - {responce.status_code}\
+                    {data_er}')
+        soup = BeautifulSoup(responce.text, 'lxml')
+        cards = soup.find_all('div', class_='w-full rounded border')
+        for card in cards:
+            title = card.find('div', class_='p-4').find('h4').find('a').text.strip()
+            price = card.find('div', class_='p-4').find('h5').text.strip()
+            card_url = f'{DETAIL_URL}{card.find('a').get('href').strip()}'
+            cards_detail.append((title, price, card_url))
+    print('Cбор данных окончен')
+    with open('scrapingclub.txt', 'w', encoding='utf8') as file:
+        for title, price, card_url in cards_detail:
+            file.writelines(f'{'#' * 20}\nНаименование и цена:\n{title} - {price}\nurl - {DETAIL_URL}{card_url}\n')
+            
+def main():
+    get_data()
+
+if __name__ == '__main__':
+    main()
