16def604a38e221cf36fd0f80f8db021b82632fb
alazev <a.lazev@transdata.no>
2023-11-27 20:44:48 +0200

    Add descr to main.py

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import xlsxwriter
 from services import get_card_data
 
 def writer(data):
+    # Создаем книгу
     book = xlsxwriter.Workbook('scrapingclub.xlsx')
+    # Создаем лист под именем одежда
     page = book.add_worksheet('одежда')
-
+    # Включаем жирный текст
     bold = book.add_format({'bold': True})
-
+    # В указанные столбцы прописываем названия 
     page.write('A1', 'Название', bold)
     page.write('B1', 'Цена', bold)
     page.write('C1', 'Описание', bold)
 
+    # Индекс в exel идет с нуля поэтому row мы указываем как начинать с 1 потому что 0 уже занят названиями столбцов
     row = 1
-    column = 1
-
+    column = 0
+    # Прописываем ширину ячеек
     page.set_column('A:A', 20)
     page.set_column('B:B', 20)
     page.set_column('C:C', 50)
 
     for item in data():
-        page.write(row, column, item[0])
-        page.write(row, column+1, item[1])
-        page.write(row, column+2, item[2])
+        page.write(row, column, item[0]) #item[0] - title
+        page.write(row, column+1, item[1]) #item[1] - price
+        page.write(row, column+2, item[2]) #item[2] - description
         row +=1
 
     book.close()
